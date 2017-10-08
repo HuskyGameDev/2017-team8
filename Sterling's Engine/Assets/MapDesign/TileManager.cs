53a97f5dfe185c41f2dcaad00c083ab89ef07c8f
Derrick Smith <derricks@mtu.edu>
2017-10-08 11:43:20 -0400

    added the 2D array and made a basic 5x5 testing map

---
+++ Sterling's Engine/Assets/MapDesign/TileManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour {
+
+    public static MapTile[,] mapTiles;
+
+	// Use this for initialization
+	void Start () {
+        mapTiles = new MapTile[5,5];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public static void addTile(MapTile mapTile, int x, int y)
+    {
+        mapTiles[x,y] = mapTile;
+    }
+}
