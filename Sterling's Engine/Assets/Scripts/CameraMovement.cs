db236732b191058f6c78fce2dd4c2a027d3e8b47
Colin <cmhogue@mtu.edu>
2017-10-11 09:59:21 -0400

    Some Edits

--- Sterling's Engine/Assets/Scripts/CameraMovement.cs
+++ Sterling's Engine/Assets/Scripts/CameraMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
+    //Speed is the rate at which the camera moves. Editable in the Script Component Window
     public int speed;
 
     // Use this for initialization
@@ -12,19 +13,18 @@ public class CameraMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //For now all that is done is moving the camer via the arrow keys.
+        //For now, relegating the movement to x or y at a time, not both. Will prioritize the keypresses.
 		if(Input.GetKey(KeyCode.RightArrow))
 		{
 			transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
-		}
-		if(Input.GetKey(KeyCode.LeftArrow))
+		}else if(Input.GetKey(KeyCode.LeftArrow))
 		{
 			transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
-		}
-		if(Input.GetKey(KeyCode.DownArrow))
+		}else if(Input.GetKey(KeyCode.DownArrow))
 		{
 			transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
-		}
-		if(Input.GetKey(KeyCode.UpArrow))
+		}else if(Input.GetKey(KeyCode.UpArrow))
 		{
 			transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
 		}
