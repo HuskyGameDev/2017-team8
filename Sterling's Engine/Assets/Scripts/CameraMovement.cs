da6742bc4c2ff48fd03df0595b6654ec8c6e077b
Colin <cmhogue@mtu.edu>
2017-10-11 10:45:18 -0400

    Edge-scrolling
    still unpolished, but functional.

--- Sterling's Engine/Assets/Scripts/CameraMovement.cs
+++ Sterling's Engine/Assets/Scripts/CameraMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour {
     //Speed is the rate at which the camera moves. Editable in the Script Component Window
     public int speed;
-
+    public int scrollZone;
     // Use this for initialization
     void Start () {
 		
@@ -28,5 +28,33 @@ public class CameraMovement : MonoBehaviour {
 		{
 			transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
 		}
-	}
+
+        print(Screen.height);
+        print(Input.mousePosition.y);
+        print(Input.mousePosition.x);
+
+        //edge-scrolling, very unpolished right now
+        //left-side of the screen. a 10th of the screen size
+        if(Input.mousePosition.x <= Screen.width / 10)
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        //right of the screen
+        if (Input.mousePosition.x >= (Screen.width * .9))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        //bottom of the screen
+        if (Input.mousePosition.y <= Screen.height / 10)
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        //top of the screen
+        if (Input.mousePosition.y >= (Screen.height * .9))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        //This will be the area for camera movement via mouse on edge
+
+    }
 }
