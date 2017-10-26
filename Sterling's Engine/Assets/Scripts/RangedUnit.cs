0863988de5386b27763af3efbcf17092adff362f
BushWookiee <jwwolosc@mtu.edu>
2017-10-26 19:30:58 -0400

    Added 3 Archtypes for Classes
    Added script files for 3 unti archetypes

---
+++ Sterling's Engine/Assets/Scripts/RangedUnit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedUnit : UnitClass {
+
+	public RangedUnit(){
+		unitClassName = "Ranged";
+		//Sprite = ""
+		Health = 3;
+		Speed = 2;
+		Range = 3;
+		Damage = 4;
+		Cost = 175;
+	}
+}
