b552fd435758569cae571d6b38d05caca041332f
Colin <cmhogue@mtu.edu>
2017-10-28 11:32:50 -0400

    Making things better?

---
+++ Sterlings' Rage/Assets/Scripts/AntiInfClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiInfClass : UnitClass {
+
+	public AntiInfClass(){
+		UnitClassName = "Anti-Infantry";
+		//Sprite = ""
+		Health = 3;
+		Speed = 3;
+		Range = 1;
+		//If not Infantry Damage = 2
+		//Else
+		Damage = 4;
+		Cost = 85;
+	}
+}
