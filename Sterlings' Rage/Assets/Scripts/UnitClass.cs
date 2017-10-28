14e73bd885f28da3b9f17df7d55515faad7339dd
tlhamilt <tlhamilt@mtu.edu>
2017-10-28 12:47:59 -0400

    added move to Unit script

--- Sterlings' Rage/Assets/Scripts/UnitClass.cs
+++ Sterlings' Rage/Assets/Scripts/UnitClass.cs
@@ -3,51 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitClass {
-	private string unitClassName;
+    private string unitClassName;
 
     // Test Comment
 
-	// Stats
-	private int health;
-	private int speed;
-	private int range;
-	private int damage;
-	private string sprite;
-	private int cost;
+    // Stats
+    private int health;
+    private int speed;
+    private int range;
+    private int damage;
+    private string sprite;
+    private int cost;
+    MapTile currentTile;
 
-	public string UnitClassName{
-		get{return unitClassName;}
-		set{unitClassName = value;}
-	}
+    public string UnitClassName {
+        get { return unitClassName; }
+        set { unitClassName = value; }
+    }
 
-	public string Sprite{
-		get{return sprite;}
-		set{sprite = value;}
-	}
+    public string Sprite {
+        get { return sprite; }
+        set { sprite = value; }
+    }
 
-	public int Health{
-		get{return health;}
-		set{health = value;}
-	}
+    public int Health {
+        get { return health; }
+        set { health = value; }
+    }
 
-	public int Speed{
-		get{return speed;}
-		set{speed = value;}
-	}
-	
-	public int Range{
-		get{return range;}
-		set{range = value;}
-	}
-	
-	public int Damage{
-		get{return damage;}
-		set{damage = value;}
-	}
+    public int Speed {
+        get { return speed; }
+        set { speed = value; }
+    }
 
-	public int Cost{
-		get{return cost;}
-		set{cost = value;}
-	}
+    public int Range {
+        get { return range; }
+        set { range = value; }
+    }
+
+    public int Damage {
+        get { return damage; }
+        set { damage = value; }
+    }
+
+    public int Cost {
+        get { return cost; }
+        set { cost = value; }
+    }
+
+    public MapTile CurrentTile{
+        get { return currentTile; }
+        set { currentTile = value; }
+    }
+
+    public void moveTo(MapTile dest) {
+        int destx = (int)dest.transform.position.x;
+        int desty = (int)dest.transform.position.y;
+        int currentx = (int)currentTile.transform.position.x;
+        int currenty = (int)currentTile.transform.position.y;
+        if(Mathf.Abs(destx - currentx) + Mathf.Abs(desty - currenty) <= range)
+        {
+            currentTile = dest;
+        }
+        
+    }
 	
 }
