571ce97dce2d46ff3b51d74b8f9cfd8d2b34d5ab
dsmith360 <derricks@mtu.edu>
2017-11-15 16:59:21 -0500

    now indicates which path the unit will take and the unit will follow that path

--- Sterlings' Rage/Assets/Scripts/UnitClass.cs
+++ Sterlings' Rage/Assets/Scripts/UnitClass.cs
@@ -16,8 +16,10 @@ public class UnitClass : MonoBehaviour {
     public int cost;
     public MapTile currentTile;
     private MapTile dest;
+    private ArrayList path;
     private bool isMovingX;
     private bool isMovingY;
+    private bool moving;
 
     public string UnitClassName {
         get { return unitClassName; }
@@ -71,51 +73,47 @@ public class UnitClass : MonoBehaviour {
 
         if (curTile.getLeft() != null)
         {
-            findPossiblePaths(curTile.getLeft(), speed - curTile.getLeft().getMovementWeight(), pathTiles);
+            findPossiblePaths(curTile.getLeft(), speed - curTile.getLeft().getMovementWeight(), pathTiles, curTile);
         }
         if (curTile.getDown() != null)
         {
-            findPossiblePaths(curTile.getDown(), speed - curTile.getDown().getMovementWeight(), pathTiles);
+            findPossiblePaths(curTile.getDown(), speed - curTile.getDown().getMovementWeight(), pathTiles, curTile);
         }
         if (curTile.getRight() != null)
         {
-            findPossiblePaths(curTile.getRight(), speed - curTile.getRight().getMovementWeight(), pathTiles);
+            findPossiblePaths(curTile.getRight(), speed - curTile.getRight().getMovementWeight(), pathTiles, curTile);
         }
         if (curTile.getUp() != null)
         {
-            findPossiblePaths(curTile.getUp(), speed - curTile.getUp().getMovementWeight(), pathTiles);
+            findPossiblePaths(curTile.getUp(), speed - curTile.getUp().getMovementWeight(), pathTiles, curTile);
         }
         TileManager.setPathList(pathTiles);
     }
 
-    private void findPossiblePaths(MapTile curTile, int speed, ArrayList pathTiles)
+    private void findPossiblePaths(MapTile curTile, int speed, ArrayList pathTiles, MapTile previous)
     {
         if (speed < 0)
         {
             return;
         }
-
-        
-        if (curTile.currentUnit == null)
-        {
-            curTile.setPossibleMove(true);
-            // Stores the speed that it was found with so that if a faster path is found it will know
-            curTile.setStoredSpeed(speed);
-            
-        }
+        curTile.setPossibleMove(true);
+        curTile.setPreviousTile(previous);
+        // Stores the speed that it was found with so that if a faster path is found it will know
+        curTile.setStoredSpeed(speed);
         pathTiles.Add(curTile);
 
+        // Recursive calls to all adjacent tiles that haven't already been checked or that have but had a lower speed when they were reached.
         if (curTile.getLeft() != null && !TileManager.containsTile(speed - curTile.getLeft().getMovementWeight(), curTile.getLeft()))
-            findPossiblePaths(curTile.getLeft(), speed - curTile.getLeft().getMovementWeight(), pathTiles);
+            findPossiblePaths(curTile.getLeft(), speed - curTile.getLeft().getMovementWeight(), pathTiles, curTile);
 
         if (curTile.getDown() != null && !TileManager.containsTile(speed - curTile.getDown().getMovementWeight(), curTile.getDown()))
-            findPossiblePaths(curTile.getDown(), speed - curTile.getDown().getMovementWeight(), pathTiles);
+            findPossiblePaths(curTile.getDown(), speed - curTile.getDown().getMovementWeight(), pathTiles, curTile);
 
         if (curTile.getRight() != null && !TileManager.containsTile(speed - curTile.getRight().getMovementWeight(), curTile.getRight()))
-            findPossiblePaths(curTile.getRight(), speed - curTile.getRight().getMovementWeight(), pathTiles);
+            findPossiblePaths(curTile.getRight(), speed - curTile.getRight().getMovementWeight(), pathTiles, curTile);
 
         if (curTile.getUp() != null && !TileManager.containsTile(speed - curTile.getUp().getMovementWeight(), curTile.getUp()))
-            findPossiblePaths(curTile.getUp(), speed - curTile.getUp().getMovementWeight(), pathTiles);
+            findPossiblePaths(curTile.getUp(), speed - curTile.getUp().getMovementWeight(), pathTiles, curTile);
     }
 
     public GameObject EnemyInRange(float range){
@@ -139,62 +137,91 @@ public class UnitClass : MonoBehaviour {
         return closest;
     }
 
-    public void MoveTo(MapTile dest) {
-        
-        isMovingX = true;
-        this.dest = dest;
-        dest.currentUnit = this;
+    public void MoveTo(ArrayList movementPath, MapTile dest)
+    {
+        if(path == null)
+            path = new ArrayList();
+        path.Clear();
+        // Have to add to the path for this unit because the tile will clear it's movement path 
+        // before it is able to travel there
+        for (int i = movementPath.Count - 1; i >= 0; i--)
+        {
+            path.Add(movementPath[i]);
+        }
+        path.Add(dest);
+        this.dest = (MapTile)path[0];
+        path.Remove(this.dest);
         currentTile.currentUnit = null;
+        currentTile = dest;
+        isMovingX = true;
+        moving = true;
     }
 
     public void Update()
     {
-        if (isMovingX)
+        if (moving)
         {
-            if (dest.transform.position.x < currentTile.transform.position.x)
+            if (isMovingX)
             {
-                if(dest.transform.position.x >= transform.position.x)
+                if (dest.transform.position.x < transform.position.x)
                 {
-                    isMovingX = false;
-                    isMovingY = true;
-                } else 
                     transform.Translate(-0.1f, 0, 0);
-            }
-            else
-            {
-                if (dest.transform.position.x <= transform.position.x)
+                    if (dest.transform.position.x >= transform.position.x)
+                    {
+                        isMovingY = true;
+                        isMovingX = false;
+                    }
+                }
+                else if (dest.transform.position.x > transform.position.x)
                 {
-                    isMovingX = false;
-                    isMovingY = true;
-                } else
                     transform.Translate(0.1f, 0, 0);
-            }
-        }
-        else if (isMovingY)
-        {
-            if(dest.transform.position.y < currentTile.transform.position.y)
-            {
-                if(dest.transform.position.y >= transform.position.y)
+                    if (dest.transform.position.x <= transform.position.x)
+                    {
+                        isMovingY = true;
+                        isMovingX = false;
+                    }
+                }
+                else
                 {
-                    isMovingY = false;
-                    currentTile = dest;
-                    // Because of how floats work need to make sure to set the units position to be the same as the tile or
-                    // sometimes it will move to far
-                    gameObject.transform.position = new Vector2(dest.transform.position.x, dest.transform.position.y);
-                    dest = null;
-                } else
+                    isMovingY = true;
+                    isMovingX = false;
+                }
+            }
+            else if (isMovingY)
+            {
+                if (dest.transform.position.y < transform.position.y)
+                {
                     transform.Translate(0, -0.1f, 0);
-            }
-            else
-            {
-                if (dest.transform.position.y <= transform.position.y)
+                    if (dest.transform.position.y >= transform.position.y)
+                    {
+                        isMovingY = false;
+                        // Because of how floats work need to make sure to set the units position to be the same as the tile or
+                        // sometimes it will move to far
+                        gameObject.transform.position = new Vector2(dest.transform.position.x, dest.transform.position.y);                        
+                    }
+                }
+                else
                 {
-                    isMovingY = false;
-                    currentTile = dest;
-                    gameObject.transform.position = new Vector2(dest.transform.position.x, dest.transform.position.y);
-                    dest = null;
-                } else
                     transform.Translate(0, 0.1f, 0);
+                    if (dest.transform.position.y <= transform.position.y)
+                    {
+                        isMovingY = false;
+                        gameObject.transform.position = new Vector2(dest.transform.position.x, dest.transform.position.y);
+                    }
+                }
+            }
+
+            if (!isMovingX && !isMovingY)
+            {
+                if (path.Count > 0)
+                {
+                    dest = (MapTile)path[0];
+                    path.Remove(dest);
+                    isMovingX = true;
+                } else
+                {
+                    moving = false;
+                }
             }
         }
     }
