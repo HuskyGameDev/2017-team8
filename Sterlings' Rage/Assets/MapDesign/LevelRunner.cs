9e800fb9a89c741f7c0eb33a6d6ed673d3050ff4
tlhamilt <tlhamilt@mtu.edu>
2017-12-16 03:30:13 -0500

    added character portraits, level framework

---
+++ Sterlings' Rage/Assets/MapDesign/LevelRunner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelRunner : MonoBehaviour {
+    public UnitClass boss;
+    public string objective;
+    public int numUnits;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (objective.Equals("defeat boss"))
+        {
+            if (boss.health <= 0)
+            {
+                print("Level Cleared");
+            }
+        }
+        else if (objective.Equals("defeat all"))
+        {
+            if (UnitManager.EnemyUnits.Count == 0)
+            {
+                print("Level Cleared");
+            }
+        }
+        else
+        {
+            //add more objectives if necessary
+        }
+    }
+}
