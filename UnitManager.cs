d90f43fb6a696c64e407f9d8e0a41d1fd5d8aac8
Jacob Woloschek <jwwolosc@mtu.edu>
2018-01-30 16:45:05 -0500

    Updated UnitManager to check for Win or Lose
    Checks the size of array lists that keep track of "player" and "enemy" units.

---
+++ UnitManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitManager : MonoBehaviour {
+
+    public static ArrayList PlayerUnits = new ArrayList();
+    public static ArrayList EnemyUnits = new ArrayList();
+    private static UnitClass selectedUnit;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            print("Enemys");
+            foreach (UnitClass enemy in EnemyUnits)
+                print(enemy.unitClassName);
+            print("PlayerUnits");
+            foreach (UnitClass unit in PlayerUnits)
+                print(unit.unitClassName);
+        }
+        // Win/Lose condition checking
+        if(PlayerUnits.Length == 0){
+            print("You Lose!");
+        }
+        if(EnemyUnits.Length == 0){
+            print("You Win!");
+        }
+    }
+
+    public static void newTurn()
+    {
+        foreach (UnitClass unit in PlayerUnits)
+            unit.newTurn();
+    }
+
+    public static void aiTurn()
+    {
+        foreach (UnitClass unit in EnemyUnits)
+            unit.newTurn();
+    }
+
+    public static void setSelectedUnit(UnitClass unit)
+    {
+        selectedUnit = unit;
+    }
+
+    public static UnitClass getSelectedUnit()
+    {
+        return selectedUnit;
+    }
+}
